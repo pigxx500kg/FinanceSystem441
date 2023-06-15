cc7a04b7a365a5cd69801b9eb115ef3a232b5236
pigxx500kg <58476995+pigxx500kg@users.noreply.github.com>
2023-06-15 18:26:42 +0800

    Change k-plot

--- FinanceSystem/CreateHalfYearK-plot.py
+++ FinanceSystem/CreateHalfYearK-plot.py
@@ -1,19 +1,20 @@
 import efinance as ef
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import mplfinance as mpf
 
-
+matplotlib.rc("font", family='Microsoft YaHei')
 # 股票代码
 
 # stock_code = '600519'
 # df = ef.stock.get_quote_history(stock_code)
-stock_name = "不好当家"
+stock_name = "好运"
 df = ef.stock.get_quote_history(stock_name)
 if(len(df)!=0):
     # 确保日期列是datetime类型
     df['日期'] = pd.to_datetime(df['日期'])
-
+    title = df.loc[0, '股票名称'] + "半年K线图"
     # 找出后半年的日期范围，以便筛选数据
     end_date = df['日期'].max()
     start_date = end_date - pd.DateOffset(months=6)
@@ -27,8 +28,8 @@ if(len(df)!=0):
 
     # 创建K线图
     mc = mpf.make_marketcolors(up='r',down='g') # 红涨绿跌
-    s  = mpf.make_mpf_style(marketcolors=mc)
-    mpf.plot(df_half_year, type='candle', style=s, ylabel='Price', volume=True)
+    s  = mpf.make_mpf_style(marketcolors=mc, rc={'font.family': 'Microsoft Yahei'})
+    mpf.plot(df_half_year, type='candle', style=s, title = title, ylabel='价格', volume=True)
     # mpf.plot(df_half_year, type='candle', style=s, title='K线图', ylabel='价格', volume=True)
 
     plt.show()
